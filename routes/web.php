b912f2703b5c1f0e7fd7d33212c299e8dcb5975c
siamshaeed <siamshaeed@gmail.com>
2022-03-07 21:40:16 +0600

    blank

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-| 
+| Siam
 */
 
 Route::get('/', function () {
